61b6e059fb8105a9b19bd1d71ac9d1bc46bdff51
xxcclong <hkz262626@outlook.com>
2023-02-10 00:56:29 +0800

    typed linear test

--- cxgnncomp/typed_linear.py
+++ cxgnncomp/typed_linear.py
@@ -1,6 +1,7 @@
 from .codegen.triton_typed_matmul import typed_matmul
 import torch
 import cxgnncomp_backend
+import torch.nn.functional as F
 
 
 class TypedLinearE2EOP(torch.autograd.Function):
@@ -37,7 +38,13 @@ class TypedLinearS2EOP(torch.autograd.Function):
     # x: [Src, dim]
 
     @staticmethod
-    def forward(ctx, x, weights, types, src_idx, preprocessed=False, count=None):
+    def forward(ctx,
+                x,
+                weights,
+                types,
+                src_idx,
+                preprocessed=False,
+                count=None):
         ctx.save_for_backward(x, weights, types, src_idx)
         if preprocessed:
             return typed_matmul(x, weights, types, src_idx, types.shape[0])
@@ -52,13 +59,12 @@ class TypedLinearS2EOP(torch.autograd.Function):
                                 device=x.device,
                                 dtype=types.dtype)
         new_types[:types.shape[0]] = types
-        new_src_idx = torch.empty(
-            [src_idx.shape[0] + num_rel * (thres - 1)],
-            device=x.device,
-            dtype=src_idx.dtype)
+        new_src_idx = torch.empty([src_idx.shape[0] + num_rel * (thres - 1)],
+                                  device=x.device,
+                                  dtype=src_idx.dtype)
         new_src_idx[:src_idx.shape[0]] = src_idx
         cxgnncomp_backend.pad_rel_idx(new_types, new_src_idx, count, thres,
-                                        num_rel, types.shape[0])
+                                      num_rel, types.shape[0])
         sorted_types, indices = torch.sort(new_types)
         output = typed_matmul(
             x,
@@ -73,3 +79,97 @@ class TypedLinearS2EOP(torch.autograd.Function):
     @staticmethod
     def backward(ctx, grad_out):
         pass
+
+
+class TypedLinearS2DMMAggrOP(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, x, weights, ptr, idx, rel, num_center):
+        ctx.save_for_backward(x, weights, ptr, idx, rel)
+        num_rel = weights.shape[0]
+        output = torch.zeros([num_center, weights.shape[-1]], device=x.device)
+        for i in range(num_rel):
+            transformed_x = torch.mm(x, weights[i])
+            cxgnncomp_backend.selective_aggr(transformed_x, ptr, idx,
+                                             (rel == i), output, num_center)
+        return output
+
+    @staticmethod
+    def backward(ctx, grad_out):
+        x, weights, ptr, idx, rel = ctx.saved_tensors
+        num_rel = weights.shape[0]
+        grad_x = torch.zeros_like(x)
+        grad_weights = []
+        num_center = grad_out.shape[0]
+        num_node = x.shape[0]
+        x_t = x.transpose(0, 1)
+        for i in range(num_rel):
+            grad_selective = torch.zeros([num_node, grad_out.shape[-1]],
+                                         device=x.device)
+            cxgnncomp_backend.selective_aggr_bwd(
+                grad_out, ptr, idx, (rel == i), grad_selective,
+                num_center)  # pass grad through selective_aggr
+            grad_x += torch.mm(grad_selective, weights[i].transpose(0, 1))
+            grad_weights.append(torch.mm(x_t, grad_selective))
+        return grad_x, torch.stack(grad_weights), None, None, None, None
+
+
+class TypedLinearS2DAggrMMOP(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, x, weights, ptr, idx, rel, num_center):
+        num_rel = weights.shape[0]
+        output = torch.zeros([num_center, weights.shape[-1]], device=x.device)
+        aggr_outputs = []
+        for i in range(num_rel):
+            aggr_output = torch.zeros([num_center, weights.shape[-2]],
+                                      device=x.device)
+            cxgnncomp_backend.selective_aggr(x, ptr, idx, (rel == i),
+                                             aggr_output, num_center)
+            output += torch.mm(aggr_output, weights[i])
+            # output += torch.empty([num_center, weights.shape[-1]],
+            #                       device=x.device)
+            aggr_outputs.append(aggr_output)
+        ctx.save_for_backward(x, weights, ptr, idx, rel,
+                              torch.stack(aggr_outputs))
+        return output
+
+    @staticmethod
+    def backward(ctx, grad_out):
+        x, weights, ptr, idx, rel, aggr_outputs = ctx.saved_tensors
+        num_rel = weights.shape[0]
+        grad_x = torch.zeros_like(x)
+        grad_weights = []
+        num_center = grad_out.shape[0]
+        for i in range(num_rel):
+            grad_mm = torch.mm(grad_out, weights[i].transpose(0, 1))
+            grad_weights.append(
+                torch.mm(aggr_outputs[i].transpose(0, 1), grad_out))
+            cxgnncomp_backend.selective_aggr_bwd(
+                grad_mm, ptr, idx, (rel == i), grad_x,
+                num_center)  # pass grad through selective_aggr
+        return grad_x, torch.stack(grad_weights), None, None, None, None
+
+
+# it can auto diff
+def TypedLinearS2DSort(x, weights, ptr, idx, rel, num_center, count=None):
+    dst = torch.repeat_interleave(torch.arange(num_center, device=x.device),
+                                  ptr[1:num_center + 1] - ptr[:num_center])
+    num_rel = weights.shape[0]
+    output = torch.zeros([num_center, weights.shape[-1]], device=x.device)
+    sorted_rel, indices = torch.sort(rel)
+    cnt = 0
+    if count is None:
+        count = torch.bincount(rel, minlength=num_rel).cpu()
+    else:
+        count = count.cpu()
+    src = idx[indices]
+    dst = dst[indices]
+    for i in range(num_rel):
+        s = src[cnt:cnt + count[i]]
+        d = dst[cnt:cnt + count[i]]
+        cnt += count[i]
+        feat = x[s]
+        transformed_feat = F.linear(feat, weights[i].T)
+        output.index_add_(0, d, transformed_feat)
+    return output
--- test/1-kernels/perf-spmm-matmul.py
+++ test/1-kernels/perf-spmm-matmul.py
@@ -29,186 +29,83 @@ def test_spmm_matmul():
                         num_rel, [idx.shape[0]],
                         dtype=torch.int32,
                         device=x.device)
+    rel_int64 = rel.to(torch.int64)
     single_weight = torch.randn([x.shape[-1], x.shape[-1]],
                                 dtype=torch.float32,
                                 device=x.device)
-
-    weights = torch.repeat_interleave(single_weight.unsqueeze(0),
-                                      num_rel,
-                                      dim=0).reshape(num_rel, x.shape[-1],
-                                                     x.shape[-1])
-    # weights = torch.randn([num_rel, x.shape[-1], x.shape[-1]],
-    #                       dtype=torch.float32,
-    #                       device=x.device)
-    # # method 1
-    # cxgc.prof(
-    #     "rgcn", "direct", lambda: cxgnncomp_backend.aggr_rgcn_direct_func(
-    #         x, ptr, idx, weights, rel, ptr.shape[0] - 1))
-    # cxgc.prof(
-    #     "rgcn", "direct", lambda: cxgnncomp_backend.aggr_rgcn_direct_func(
-    #         x, ptr, idx, weights, rel, ptr.shape[0] - 1))
-
-    # method 2
-    gt_output_dst = cxgc.RGCNOP2.apply(x, weights, ptr, idx, rel,
-                                       ptr.shape[0] - 1)
-    cxgc.prof(
-        "rgcn", "pre-transform", lambda: cxgc.RGCNOP2.apply(
-            x, weights, ptr, idx, rel, ptr.shape[0] - 1))
-
-    # method 3
     dst = torch.repeat_interleave(
         torch.arange(ptr.shape[0] - 1, device=x.device), ptr[1:] - ptr[:-1])
+    x_edge = x[idx]
     count = torch.bincount(rel, ).cpu()
     print(count)
-    cxgc.prof(
-        "rgcn", "sort mm", lambda: cxgc.RGCNOP_sorted(
-            x,
-            weights,
-            idx,
-            dst,
-            count,
-            ptr.shape[0] - 1,
-        ))
 
-    # profile sort
-    def sort_and_move(x, rel):
-        sorted, indices = torch.sort(rel)
-        x_edge = x[idx[indices]]
-        count = torch.bincount(rel).cpu()
+    # weights = torch.repeat_interleave(single_weight.unsqueeze(0),
+    #                                   num_rel,
+    #                                   dim=0).reshape(num_rel, x.shape[-1],
+    #                                                  x.shape[-1])
+    weights = torch.randn([num_rel, x.shape[-1], x.shape[-1]],
+                          dtype=torch.float32,
+                          device=x.device)
 
-    def sort_only(x, rel):
-        sorted, indices = torch.sort(rel)
-
-    cxgc.prof("rgcn", "sort and move", lambda: sort_and_move(x, rel))
-
-    cxgc.prof("rgcn", "sort only", lambda: sort_only(x, rel))
-
-    # method 4
-    output = torch.empty([x.shape[0], weights.shape[-1]], device=x.device)
-    v_rel = torch.randint(0, num_rel, [x.shape[0]], device=x.device)
-    cxgc.prof(
-        "rgcn", "typed_linear", lambda: cxgnncomp_backend.typed_linear(
-            x, weights, output, v_rel.int(), 32))
-
-    indexed_x = torch.index_select(x, 0, idx)
-    output = torch.empty([indexed_x.shape[0], weights.shape[-1]],
-                         device=x.device)
-    cxgc.prof(
-        "rgcn", "typed_linear", lambda: cxgnncomp_backend.typed_linear(
-            indexed_x, weights, output, rel.int(), 32))
-
-    cxgc.prof(
-        "rgcn", "typed_linear", lambda: cxgnncomp_backend.typed_linear(
-            indexed_x, weights, output, rel.int(), 64))
-
-    gt_output_edge = torch.empty([indexed_x.shape[0], weights.shape[-1]],
-                                 device=x.device)
-    cxgnncomp_backend.typed_linear(indexed_x, weights, gt_output_edge,
-                                   rel.int(), 64)
-
-    # method 5
-    count = torch.bincount(rel, ).cpu()
-
-    def aligh_rel(rel, count, thres):
-        arr = []
-        for it, r in enumerate(count):
-            if r % thres != 0:
-                toadd = thres - r % thres
-                arr += ([it for _ in range(toadd)])
-        arr = torch.Tensor(arr).cuda()
-        return torch.cat([rel, arr], dim=0)
-
-    cxgc.prof("rgcn", "align-rel", lambda: aligh_rel(rel, count, 32))
-
-    # method 6
-    def new_typed_linear(x, weights, old_idx, old_rel, num_edge):
-        thres = 256
-        count = torch.bincount(old_rel, )
-        rel = torch.empty([old_rel.shape[0] + num_rel * (thres - 1)],
-                          device=x.device,
-                          dtype=old_rel.dtype)
-        rel[:old_rel.shape[0]] = old_rel
-        idx = torch.empty([old_idx.shape[0] + num_rel * (thres - 1)],
-                          device=x.device,
-                          dtype=old_idx.dtype)
-        idx[:old_idx.shape[0]] = old_idx
-        # rel = old_rel.resize_(num_edge + num_rel * (thres - 1))
-        # idx = old_idx.resize_(num_edge + num_rel * (thres - 1))
-        cxgnncomp_backend.pad_rel_idx(rel, idx, count, thres, num_rel,
-                                      num_edge)
-        sorted_rel, indices = torch.sort(rel)
-        # cxgc.prof(
-        #     "rgcn", "typed_matmul", lambda: cxgc.codegen.typed_matmul(
-        #         x, weights, idx[indices], indices, sorted_rel))
-        # print(idx[indices])
-        output = cxgc.codegen.typed_matmul(x, weights, sorted_rel,
-                                           old_rel.shape[0], idx[indices],
-                                           indices)
-
-        return output
-
-    rel = rel.to(torch.int64)
-    output_edge = new_typed_linear(x, weights, idx, rel, num_edge=idx.shape[0])
-    output_edge = output_edge[:num_edge]
+    # method 2
+    output_dst1 = cxgc.TypedLinearS2DMMAggrOP.apply(x, weights, ptr, idx, rel,
+                                                    ptr.shape[0] - 1)
+    output_dst2 = cxgc.TypedLinearS2DAggrMMOP.apply(x, weights, ptr, idx, rel,
+                                                    ptr.shape[0] - 1)
+    output_dst3 = cxgc.TypedLinearS2DSort(x, weights, ptr, idx, rel,
+                                          ptr.shape[0] - 1)
     print(
-        torch.allclose(output_edge[:num_edge],
-                       gt_output_edge,
-                       atol=1e-2,
-                       rtol=1e-2))
-    print(output_edge[:num_edge][torch.isclose(
-        output_edge[:num_edge], gt_output_edge, atol=1e-2, rtol=1e-2) ==
-                                 False])
-    print(gt_output_edge[torch.isclose(
-        output_edge[:num_edge], gt_output_edge, atol=1e-2, rtol=1e-2) ==
-                         False])
-    print(gt_output_edge[torch.isclose(
-        output_edge[:num_edge], gt_output_edge, atol=1e-2, rtol=1e-2) ==
-                         False].shape)
-    print(gt_output_edge.shape)
-    # print(output_edge[:num_edge], gt_output_edge)
-    # print(torch.matmul(x, weights[0]))
-    # print(torch.matmul(x[idx], weights[0]))
-    # print(torch.matmul(x[0], weights[0]))
-    # print(idx)
+        "correct rate output_dst1 vs output_dst2:",
+        torch.sum(torch.isclose(output_dst1, output_dst2, atol=1e-2,
+                                rtol=1e-2)) / torch.numel(output_dst1))
+    print(
+        "correct rate output_dst1 vs output_dst3:",
+        torch.sum(torch.isclose(output_dst1, output_dst3, atol=1e-2,
+                                rtol=1e-2)) / torch.numel(output_dst1))
+    output_edge1 = cxgc.TypedLinearE2EOP.apply(x_edge, weights, rel_int64)
+    output_edge2 = cxgc.TypedLinearE2EOP.apply(x_edge, weights, rel_int64,
+                                               False, count)
+    output_edge3 = cxgc.TypedLinearS2EOP.apply(x, weights, rel_int64, idx)
+    output_edge4 = cxgc.TypedLinearS2EOP.apply(x, weights, rel_int64, idx,
+                                               False, count)
+    print(
+        "correct rate output_edge1 vs output_edge2:",
+        torch.sum(
+            torch.isclose(output_edge1, output_edge2, atol=1e-2, rtol=1e-2)) /
+        torch.numel(output_edge1))
+    print(
+        "correct rate output_edge1 vs output_edge3:",
+        torch.sum(
+            torch.isclose(output_edge1, output_edge3, atol=1e-2, rtol=1e-2)) /
+        torch.numel(output_edge1))
+    print(
+        "correct rate output_edge1 vs output_edge4:",
+        torch.sum(
+            torch.isclose(output_edge1, output_edge4, atol=1e-2, rtol=1e-2)) /
+        torch.numel(output_edge1))
 
     cxgc.prof(
-        "rgcn", "new_typed_linear",
-        lambda: new_typed_linear(x, weights, idx, rel, num_edge=idx.shape[0]))
-
-    output_dst = torch.zeros([num_center, weights.shape[-1]], device=x.device)
-    cxgc.prof("rgcn", "index add",
-              lambda: output_dst.index_add_(0, dst, output_edge[:num_edge]))
-
-    output_edge = cxgc.TypedLinearE2EOP.apply(x[idx], weights, rel)
-    print(output_edge[:num_edge][torch.isclose(
-        output_edge[:num_edge], gt_output_edge, atol=1e-2, rtol=1e-2) ==
-                                 False])
-    print(output_edge[:num_edge][torch.isclose(
-        output_edge[:num_edge], gt_output_edge, atol=1e-2, rtol=1e-2) ==
-                                 False].shape)
-
-    x_edge = x[idx]
-    cxgc.prof("rgcn", "kernel e2e",
-              lambda: cxgc.TypedLinearE2EOP.apply(x_edge, weights, rel))
-
-    count = torch.bincount(rel, )
+        "typed linear",
+        "s2d aggr mm", lambda: cxgc.TypedLinearS2DMMAggrOP.apply(
+            x, weights, ptr, idx, rel, ptr.shape[0] - 1))
     cxgc.prof(
-        "rgcn",
-        "kernel e2e with gpu count", lambda: cxgc.TypedLinearE2EOP.apply(
-            x_edge, weights, rel, False, count))
-
-    count = torch.bincount(rel, ).cpu()
+        "typed linear",
+        "s2d mm aggr", lambda: cxgc.TypedLinearS2DAggrMMOP.apply(
+            x, weights, ptr, idx, rel, ptr.shape[0] - 1))
     cxgc.prof(
-        "rgcn",
-        "kernel e2e with cpu count", lambda: cxgc.TypedLinearE2EOP.apply(
-            x_edge, weights, rel, False, count))
+        "typed linear", "s2d sort", lambda: cxgc.TypedLinearS2DSort(
+            x, weights, ptr, idx, rel, ptr.shape[0] - 1))
 
-    cxgc.prof("rgcn", "kernel s2e",
-              lambda: cxgc.TypedLinearS2EOP.apply(x, weights, rel, idx))
-
+    cxgc.prof("typed linear", "e2e",
+              lambda: cxgc.TypedLinearE2EOP.apply(x_edge, weights, rel_int64))
     cxgc.prof(
-        "rgcn", "kernel s2e", lambda: cxgc.TypedLinearS2EOP.apply(
-            x, weights, rel, idx, False, count))
+        "typed linear", "e2e with count", lambda: cxgc.TypedLinearE2EOP.apply(
+            x_edge, weights, rel_int64, False, count))
+    cxgc.prof("typed linear", "s2e",
+              lambda: cxgc.TypedLinearS2EOP.apply(x, weights, rel_int64, idx))
+    cxgc.prof(
+        "typed linear", "s2e with count", lambda: cxgc.TypedLinearS2EOP.apply(
+            x, weights, rel_int64, idx, False, count))
 
 
 if __name__ == "__main__":
