91a1a0a443a1296daa1cf24f4ba6d2570af482b2
xxcclong <hkz262626@outlook.com>
2023-03-11 14:53:03 +0800

    change data preparation

--- cxgnncomp/data_preparation.py
+++ cxgnncomp/data_preparation.py
@@ -17,7 +17,8 @@ def prepare_data():
     return x, ptr, idx, batch
 
 
-def prepare_data_full_graph(dset, feat_len=128, num_head=1):
+def prepare_data_full_graph(dset="arxiv", feat_len=128, num_head=1, need_edge_index=False):
+    print(f"=======\nLoading full graph structure... dataset={dset} feature length={feat_len} num_head={num_head}\n=======")
     ptr, idx = cxgnndl.load_full_graph_structure(dset)
     ptr = torch.from_numpy(ptr).cuda()
     idx = torch.from_numpy(idx).cuda()
@@ -29,18 +30,20 @@ def prepare_data_full_graph(dset, feat_len=128, num_head=1):
         x = torch.randn([ptr.shape[0] - 1, num_head, feat_len],
                         dtype=torch.float32,
                         device='cuda')
-
-    # edge_index = torch.stack([
-    #     idx,
-    #     torch.repeat_interleave(torch.arange(
-    #         0, ptr.shape[0] - 1, device="cuda"),
-    #                             repeats=ptr[1:] - ptr[:-1])
-    # ],
-    #                          dim=0)
     batch = {}
     batch["num_node_in_layer"] = torch.tensor([ptr.shape[0] - 1] * 4)
     batch["num_edge_in_layer"] = torch.tensor([idx.shape[0]] * 4)
-    return x, ptr, idx, batch
+    if need_edge_index:
+        edge_index = torch.stack([
+            idx,
+            torch.repeat_interleave(torch.arange(
+                0, ptr.shape[0] - 1, device="cuda"),
+                                    repeats=ptr[1:] - ptr[:-1])
+        ],
+                                dim=0)
+        return x, ptr, idx, batch, edge_index
+    else:
+        return x, ptr, idx, batch
 
 
 def prepare_data_sampled_graph(dset,
